2649f491bb6877ea19b65e39de40ac94f52bbfc3
PokNim <m.tasnimsyamim46@gmail.com>
2025-06-14 21:33:43 +0800

    New updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,6 @@ document.addEventListener('DOMContentLoaded', function() {
   });
   document.getElementById('download-full').addEventListener('click', function(e) {
     e.preventDefault();
-    window.open('https://how2sign.github.io/full_dataset.zip', '_blank'); // Replace with your full dataset link
+    window.open('https://drive.google.com/drive/folders/1b44aycPedjL5ZYj2fWt2c7ZNEUdVpQ5k?usp=drive_link', '_blank'); // Replace with your full dataset link
   });
 });
